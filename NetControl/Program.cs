9fcf4e8eac4a2846728c54b26dc058f6fdecf278
Hüseyin Dalgıç <hsyndlgc5334@hotmail.com>
2021-02-07 15:33:56 +0300

    NetControlApp

---
+++ NetControl/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace NetControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Ping ping = new Ping();
+            PingReply pingReply = ping.Send(IPAddress.Parse("93.89.238.98"));
+            System.Console.WriteLine($"Ping: {pingReply.RoundtripTime} ms");
+            System.Console.WriteLine($"Pinglenen Adres: {pingReply.Address}");
+            if(pingReply.Status==IPStatus.Success)
+            {
+                System.Console.WriteLine("İnternet bağlantısı var");
+            }
+            else
+            {
+                System.Console.WriteLine("İnternet bağlantısı yok");
+            }
+        }
+    }
+}
