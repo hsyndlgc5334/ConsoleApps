2e718baf9591f850827aab891138588c32c6629a
Hüseyin Dalgıç <hsyndlgc5334@hotmail.com>
2021-03-07 16:22:38 +0300

    CreateFindPublicIp

---
+++ FindPublicIp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetIPAddress();
+        }
+        static void GetIPAddress()
+        {
+            String address = "";
+            WebRequest request = WebRequest.Create("http://checkip.dyndns.org/");
+            using (WebResponse response = request.GetResponse())
+            using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+            {
+                address = stream.ReadToEnd();
+            }
+
+            int first = address.IndexOf("Address: ") + 9;
+            int last = address.LastIndexOf("</body>");
+            address = address.Substring(first, last - first);
+            System.Console.WriteLine($"Public ip: {address}");
+        }
+    }
+}
