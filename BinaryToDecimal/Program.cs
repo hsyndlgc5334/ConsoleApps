6c5014bf5e36047639b71ea496bffce69f37031e
Hüseyin Dalgıç <hsyndlgc5334@hotmail.com>
2021-03-15 21:54:18 +0300

    15.03.2021

---
+++ BinaryToDecimal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BinaryToDecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // try
+            // {
+            //     System.Console.Write("2'lik sayı girin: ");
+            //     string s = Console.ReadLine();
+            //     int b = Convert.ToInt32(s, 2);
+            //     Console.WriteLine("Onluğa çevrilmiş hali = " + b);
+            // }
+            // catch (Exception e)
+            // {
+            //     System.Console.WriteLine(e.Message);
+            // }
+            var p = StringReverse("hüseyin");
+            System.Console.WriteLine();
+        }
+
+        static string StringReverse(string number)
+        {
+            char[] array = number.ToCharArray();
+            Array.Reverse(array);
+            string s =
+            return s;
+        }
+    }
+}
---
+++ SpeedTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SpeedTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string tempfile = "tempfile.tmp";
+            System.Net.WebClient webClient = new System.Net.WebClient();
+
+            Console.WriteLine("Downloading file....");
+
+            System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
+            webClient.DownloadFile("http://dl.google.com/googletalk/googletalk-setup.exe", tempfile);
+            sw.Stop();
+
+            System.IO.FileInfo fileInfo = new System.IO.FileInfo(tempfile);
+            long speed = fileInfo.Length / sw.Elapsed.Seconds;
+
+            Console.WriteLine("Download duration: {0}", sw.Elapsed);
+            Console.WriteLine("File size: {0}", fileInfo.Length.ToString("N0"));
+            Console.WriteLine("Speed: {0} bps ", speed.ToString("N0"));
+
+            Console.WriteLine("Press any key to continue...");
+        }
+    }
+}
